b58469e896de48dcec070ec1613c5d1ef56c3120
talk2dennis <adigwedennis@yahoo.com>
2024-07-22 16:25:46 +0100

    added logo id and loaded image

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,22 @@
+import $ from 'jquery';
+import _ from 'lodash';
+import '../css/main.css';
+
+let count = 0;
+
+$("<div id='logo'> </div>").appendTo('body');
+$("<p>Holberton Dashboard</p>").appendTo('body');
+$("<p>Dashboard data for the students</p>").appendTo('body');
+$("<button>Click here to get started</button>").appendTo('body');
+$("<p id='count'></p>").appendTo('body');
+$("<p>Copyright - Holberton School</p>").appendTo('body');
+
+function updateCounter() {
+    count++;
+    $('#count').html(`${count} clicks on the button`);
+}
+
+$("button").on("click", _.debounce(updateCounter, 500));
+
+
+
---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/dashboard_main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images/',
+              publicPath: 'images/',
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  // Remove splitChunks unless necessary
+  optimization: {
+    splitChunks: false, // Disable splitChunks for a single bundle
+  },
+  performance: {
+    hints: false, // Optionally disable performance hints
+  },
+};
