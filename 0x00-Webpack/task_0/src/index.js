6e5daa42b985f5371b230a8037e654168c8770db
talk2dennis <adigwedennis@yahoo.com>
2024-07-22 11:34:49 +0100

    created my first webpack project

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+
+$(function() {
+    $('body').append('<p>Hello World</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<p>Copyright - Holberton School</p>');
+});
