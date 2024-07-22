cdd586bac17377e650f48ce02613afd8940e1edd
talk2dennis <adigwedennis@yahoo.com>
2024-07-22 14:55:46 +0100

    updated: added a click binded with lodash

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -2,20 +2,19 @@ import $ from 'jquery';
 import _ from 'lodash';
 
 let count = 0;
+
+$("<p>Holberton Dashboard</p>").appendTo('body');
+$("<p>Dashboard data for the students</p>").appendTo('body');
+$("<button>Click here to get started</button>").appendTo('body');
+$("<p id='count'></p>").appendTo('body');
+$("<p>Copyright - Holberton School</p>").appendTo('body');
+
 function updateCounter() {
     count++;
-    $('#count').text(`${count} clicks on the button`);
+    $('#count').html(`${count} clicks on the button`);
 }
 
-// Debounce the updateCounter function
-const debouncedUpdateCounter = _.debounce(updateCounter, 300);
+$("button").on("click", _.debounce(updateCounter, 500));
 
-$(document).ready(function() {
-    $('body').append('<p>Holberton Dashboard</p>');
-    $('body').append('<p>Dashboard data for the students</p>');
-    $('body').append('<button>Click here to get started<button>');
-    $('body').append("<p id='count'></p>");
-    $('body').append("<p>Copyright - Holberton School</p>");
-    $('button').on('click', debouncedUpdateCounter);
-});
+
 
