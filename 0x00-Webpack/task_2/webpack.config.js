09cc03d3f69304c150a4e4b775c169d7a1f4f992
talk2dennis <adigwedennis@yahoo.com>
2024-07-23 11:39:57 +0100

    updated the output config

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -3,8 +3,8 @@ const path = require('path');
 module.exports = {
   entry: './js/dashboard_main.js',
   output: {
-    filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
   },
   mode: 'production',
   module: {
