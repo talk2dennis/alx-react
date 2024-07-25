d539a8255bf5fafe0bece147566fa7d9c94a13cb
talk2dennis <adigwedennis@yahoo.com>
2024-07-25 11:13:21 +0100

    wrote the enzyme test using shallow

--- 0x01-react_intro/task_3/dashboard/src/utils.test.js
+++ 0x01-react_intro/task_3/dashboard/src/utils.test.js
@@ -1,15 +1,18 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Notifications from './Notifications'
 import { getFullYear, getFooterCopy, getLatestNotification } from './utils';
 
 describe('Utils and expects the functions to return the right text', () => {
     it('getFullYear', () => {
-        expect(getFullYear()).toEqual(new Date().getFullYear());
+        expect(getFullYear()).toBe(new Date().getFullYear());
     });
-    
+
     it('getFooterCopy', () => {
         expect(getFooterCopy(true)).toBe('Holberton School');
         expect(getFooterCopy(false)).toBe('Holberton School main dashboard');
     });
-    
+
     it('getLatestNotification', () => {
         expect(getLatestNotification()).toBe('<strong>Urgent requirement</strong> - complete by EOD');
     });
