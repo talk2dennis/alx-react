a6802f58b898e43fe85704c5a39717641c1a8504
talk2dennis <adigwedennis@yahoo.com>
2024-07-26 17:31:14 +0100

    added test case

---
+++ 0x02-react_props/task_1/dashboard/src/App/App.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+
+
+describe('<App />', () => {
+    it('renders without crashing', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper).toHaveLength(1);
+    });
+
+    it('renders a div with the class App-header', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('.App-header')).toHaveLength(1);
+    });
+
+    it('renders a div with the class App-body', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('.App-body')).toHaveLength(1);
+    });
+
+    it('renders a div with the class App-footer', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('.App-footer')).toHaveLength(1);
+    });
+});
