fe9f783e03e731fc6588c3b6930e74a2a4c3c6a3
talk2dennis <adigwedennis@yahoo.com>
2024-07-27 08:31:54 +0100

    created the notification item and test

---
+++ 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Notifications.css';
+
+
+export default function Notifications() {
+    return (
+        <div className='Notifications'>
+            <p>Here is the list of notifications</p>
+            <ul>
+                <li>New course available</li>
+                <li>New resume available</li>
+                <li><strong>Urgent requirement</strong> - complete by EOD</li>
+            </ul>
+        </div>
+    )
+}
