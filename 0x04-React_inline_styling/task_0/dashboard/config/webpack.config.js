1f03712fe45ae609b94534a9beb9661b96a77b76
talk2dennis <adigwedennis@yahoo.com>
2024-08-05 15:50:28 +0100

    styled the courselistrow using inline styling in react

---
+++ 0x04-React_inline_styling/task_0/dashboard/config/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    devtool: "inline-source-map",
+    entry: "./src/index.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve("./dist"),
+    },
+    devServer: {
+        hot: true,
+        static: path.resolve("./dist"),
+        compress: true,
+        port: 8564,
+        historyApiFallback: true,
+    },
+    performance: {
+        maxAssetSize: 1000000,
+        maxEntrypointSize: 1000000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    "file-loader",
+                    {
+                        loader: "image-webpack-loader",
+                        options: {
+                            bypassOnDebug: true, 
+                            disable: true, 
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+};
