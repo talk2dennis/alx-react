7cf700e7374333e8641d31b9b5dc4f8aa92bdb9b
talk2dennis <adigwedennis@yahoo.com>
2024-08-22 14:01:38 +0100

    created normalizr schema for notifications

--- 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
+++ 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -1,8 +1,24 @@
-import * as notification from '../../notifications.json';
+import * as notificationData from '../../notifications.json';
+import { normalize, schema } from "normalizr";
 
+// Define the user, message, and notification schemas
+const user = new schema.Entity('users');
+const message = new schema.Entity('messages', {}, { idAttribute: 'guid' });
+const notification = new schema.Entity('notifications', {
+    author: user,
+    context: message
+});
 
-export default getAllNotificationsByUser = (userId) => {
-    const notifications = notification.default;
-    const userNotifications = Object.values(notifications).filter((notification) => notification.author.id === userId);
-    return userNotifications;
-};
+// Normalize the notification data
+const normalizedData = normalize(notificationData.default, [notification]);
+
+// Function to get all notifications for a specific user by user ID
+export default function getAllNotificationsByUser(userId) {
+    const notifications = normalizedData.entities.notifications;
+    return Object.values(notifications)
+        .filter(notification => notification.author === userId)
+        .map(notification => normalizedData.entities.messages[notification.context]);
+}
+
+// Export the normalized data
+export { normalizedData };
