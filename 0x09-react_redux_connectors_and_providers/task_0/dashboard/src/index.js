609e5cc233d180027b11b6a0dd82eb6dea195ffa
talk2dennis <adigwedennis@yahoo.com>
2024-08-30 06:21:11 +0100

    created a store and a provider for the app

--- 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import App from './App/App';
+import uiReducer from './reducers/uiReducer';
+import { Map } from 'immutable';
+
+
+const store = createStore(uiReducer, Map());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>,
   </React.StrictMode>
 );
